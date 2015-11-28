26dcdb891a9eb0a17aa0172dfc3d5e36efe0c4f6
Peter Petrov <petrov.p.peter@gmail.com>
2015-11-28 15:11:45 -0500

    switched to jade

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-/*global require*/
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -14,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
