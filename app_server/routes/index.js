375665bbe2254b8fa6fe8e169a504ae53f98ebe2
Peter Petrov <petrov.p.peter@gmail.com>
2015-11-29 19:38:43 -0500

    bower and grunt

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
+var ctrlGames = require('../controllers/games');
+var ctrlModes = require('../controllers/modes');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
+
+router.get('/games/', ctrlGames.index);
+
+router.get('/games/:game_id/modes/', ctrlModes.index);
 
 module.exports = router;
