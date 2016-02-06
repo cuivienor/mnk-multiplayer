13aaaa527d4d6d8ff325ed12f04fe2f683ed3942
Peter Petrov <petrov.p.peter@gmail.com>
2016-02-06 14:02:17 -0500

    schema now respects spec

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,3 @@
-INSERT INTO games (name, type, params) VALUES ('Tic-Tac-Toe', 'mnk', '{"m":3,"n":3,"k":3,"maxPlayers":2}');
-INSERT INTO games (name, type, params) VALUES ('Gomoku', 'mnk', '{"m":15,"n":15,"k":5,"maxPlayers":2}');
-INSERT INTO games (name, type, params) VALUES ('Connect6', 'mnk', '{"m":15,"n":15,"k":6,"maxPlayers":2}');
+INSERT INTO games (name, type, spec) VALUES ('Tic-Tac-Toe', 'mnk', '{"m":3,"n":3,"k":3,"maxPlayers":2}');
+INSERT INTO games (name, type, spec) VALUES ('Gomoku', 'mnk', '{"m":15,"n":15,"k":5,"maxPlayers":2}');
+INSERT INTO games (name, type, spec) VALUES ('Connect6', 'mnk', '{"m":15,"n":15,"k":6,"maxPlayers":2}');
