fa1cedef288a327ca8365ecc03444fca28b4cd46
Peter Petrov <petrov.p.peter@gmail.com>
2016-02-06 12:38:13 -0500

    changes schema

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,3 @@
-INSERT INTO games (name, type, params) VALUES ('Tic-Tac-Toe', 'mnk', '3-3-3-1-1');
-INSERT INTO games (name, type, params) VALUES ('Gomoku', 'mnk', '15-15-5-1-1');
-INSERT INTO games (name, type, params) VALUES ('Connect6', 'mnk', '15-15-6-1-2');
+INSERT INTO games (name, type, params) VALUES ('Tic-Tac-Toe', 'mnk', '{"m":3,"n":3,"k":3,"maxPlayers":2}');
+INSERT INTO games (name, type, params) VALUES ('Gomoku', 'mnk', '{"m":15,"n":15,"k":5,"maxPlayers":2}');
+INSERT INTO games (name, type, params) VALUES ('Connect6', 'mnk', '{"m":15,"n":15,"k":6,"maxPlayers":2}');
