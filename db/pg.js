dcfd34d08f84fd9c32f11182f3e6a41b2763f2d5
Peter Petrov <petrov.p.peter@gmail.com>
2016-02-10 21:07:58 -0500

    Added a state, which keeps all challanges in local memory as initialized games. It\'s hacky so def need refactoring

--- db/pg.js
+++ db/pg.js
@@ -26,17 +26,27 @@ module.exports.renderGames = function (res) {
     });
 };
 
-module.exports.writeChallange = function (params, res) {
+module.exports.writeChallange = function (params, res, addGame) {
     console.log(params);
     pg.connect(conString, function(err, client, done) {
 
         if(handleError(err, client, done)) return;
 
-        client.query('INSERT INTO challanges (url, game_id) VALUES ($2, $1)', params, function(err, result) {
+        client.query('INSERT INTO challenges (url, game_id) VALUES ($2, $1)', params, function(err, result) {
             if (handleError(err, client, done)) return;
             res.send({id: params[1]});
             done();
         });
+
+        var url = params[1];
+        client.query('SELECT games.spec FROM (SELECT * FROM challenges WHERE url = $1) T LEFT JOIN games ON T.game_id = games.id', [url], function(err, result) {
+            if (handleError(err, client, done)) return;
+
+            var spec = JSON.parse(result.rows[0].spec);
+            addGame(url, new MnkGame(spec));
+            done();
+        });
+        
         
     });
 };
@@ -47,7 +57,7 @@ module.exports.getGame = function (url, res) {
 
         if(handleError(err, client, done)) return;
 
-        client.query('SELECT games.name, games.type, games.spec FROM (SELECT * FROM challanges WHERE url = $1) T LEFT JOIN games ON T.game_id = games.id', [url], function(err, result) {
+        client.query('SELECT games.name, games.type, games.spec FROM (SELECT * FROM challenges WHERE url = $1) T LEFT JOIN games ON T.game_id = games.id', [url], function(err, result) {
             if (handleError(err, client, done)) return;
 
             var spec = JSON.parse(result.rows[0].spec);
@@ -66,7 +76,7 @@ module.exports.initiateGame = function (url, game, player, io) {
 
         if(handleError(err, client, done)) return;
 
-        client.query('SELECT games.name, games.type, games.spec FROM (SELECT * FROM challanges WHERE url = $1) T LEFT JOIN games ON T.game_id = games.id', [url], function(err, result) {
+        client.query('SELECT games.name, games.type, games.spec FROM (SELECT * FROM challenges WHERE url = $1) T LEFT JOIN games ON T.game_id = games.id', [url], function(err, result) {
             if (handleError(err, client, done)) return;
 
             var spec = JSON.parse(result.rows[0].spec);
@@ -81,20 +91,37 @@ module.exports.initiateGame = function (url, game, player, io) {
 
 };
 
-module.exports.populateGames = function(games) {
+module.exports.populateGames = function(addGame) {
     
     pg.connect(conString, function(err, client, done) {
 
         if(handleError(err, client, done)) return;
 
-        client.query('SELECT games.spec, challanges.url FROM challanges LEFT JOIN games ON challanges.game_id = games.id', function(err, result) {
+        client.query('SELECT games.spec, challenges.url FROM challenges LEFT JOIN games ON challenges.game_id = games.id', function(err, result) {
             result.rows.forEach(function(game) {
-                games[game.url] = new MnkGame(game.spec);
+                addGame(game.url, new MnkGame(JSON.parse(game.spec)));
             });
-            console.log(games);
         });
     });
     
+};
+
+module.exports.addGame = function(url, games) {
+    pg.connect(conString, function(err, client, done) {
+
+        if(handleError(err, client, done)) return;
+
+        client.query('SELECT games.spec FROM (SELECT * FROM challenges WHERE url = $1) T LEFT JOIN games ON T.game_id = games.id', [url], function(err, result) {
+            if (handleError(err, client, done)) return;
+
+            var spec = JSON.parse(result.rows[0].spec);
+            console.log(result.rows);
+            games[url] = new MnkGame(spec);
+            done();
+        });
+
+    });
+
 };
 
 
--- io.js
+++ io.js
@@ -1,9 +1,10 @@
 var io = require('socket.io')();
 var pg = require('./db/pg');
 var Player = require('./lib/player.js');
-var games = {};
+var state = require('./lib/state.js');
 
-pg.populateGames(games);
+pg.populateGames(state.addGame);
+state.setRadios(io);
 
 io.on('connection', function(socket) {
     console.log('user connected');
@@ -13,19 +14,16 @@ io.on('connection', function(socket) {
         console.log('user disconnected');
     });
 
-    // socket.on('register', function(url) {
-    //     socket.join(url);
-    //     if (currentGames[url] === undefined) {
-    //         pg.initiateGame(url, currentGames, player, io);
-    //     } else {
-    //         player.addToGame(currentGames[url]);
-    //     }
-    // });
+    socket.on('register', function(url) {
+        socket.join(url);
+        player.addToGame(state.getGames()[url]);
+        state.setRadios(io);
+    });
 
-    // socket.on('move', function(moveSpec) {
-    //     player.move(moveSpec);
-    //     console.log(player.game.board);
-    // });
+    socket.on('move', function(moveSpec) {
+        player.move(moveSpec);
+        console.log(player.game.board);
+    });
 });
 
 module.exports = io;
